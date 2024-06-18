38c01f39198b46ef622d3714b4de1c6de705906d
Larissa Faria <lari.f4ria@gmail.com>
2024-06-18 11:48:36 -0300

    feat: edited buttons layout from multiple selection

--- app/src/main/java/br/com/fiap/emailapp/pages/HomeScreen.kt
+++ app/src/main/java/br/com/fiap/emailapp/pages/HomeScreen.kt
@@ -1,5 +1,6 @@
 package br.com.fiap.emailapp.pages
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,9 +10,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -22,9 +29,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import br.com.fiap.emailapp.R
 import br.com.fiap.emailapp.components.EmailButton
 import br.com.fiap.emailapp.components.EmailComp
 import br.com.fiap.emailapp.components.EmailListViewModel
@@ -61,8 +73,14 @@ fun HomeScreen(navController: NavHostController, viewModel: EmailListViewModel, 
         modifier = Modifier.fillMaxSize()
     ) {
         Row {
-            TextButton(onClick = { multipleSelection = !multipleSelection }) {
-                Text(text = ".")
+            IconButton(
+                onClick = { multipleSelection = !multipleSelection },
+            ) {
+                Icon(
+                    imageVector = ImageVector.vectorResource(id = R.drawable.checklist_icon),
+                    contentDescription = "multiple selection",
+                    tint = MaterialTheme.colorScheme.primary
+                )
             }
             FilterComp(filterLabel, onFilterChange = { newLabel ->
                 filterLabel = newLabel
@@ -71,17 +89,45 @@ fun HomeScreen(navController: NavHostController, viewModel: EmailListViewModel, 
 
         if (multipleSelection){
             Spacer(modifier = Modifier.height(5.dp))
-            Row {
-                Button(onClick = { multipleSelection = !multipleSelection }) {
-                    Text(text = "<-")
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ){
+                IconButton(
+                    onClick = { multipleSelection = !multipleSelection },
+                    modifier = Modifier.background(MaterialTheme.colorScheme.secondary, CircleShape).clip(
+                        CircleShape)
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.return_icon),
+                        contentDescription = "voltar",
+                        tint = Color.White,
+                        modifier = Modifier.size(20.dp)
+                    )
                 }
-
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "arquivar")
+                IconButton(
+                    onClick = {  },
+                    modifier = Modifier.background(MaterialTheme.colorScheme.secondary, CircleShape).clip(
+                        CircleShape)
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.archive_icon),
+                        contentDescription = "arquivar",
+                        tint = Color.White,
+                        modifier = Modifier.size(20.dp)
+                    )
                 }
-
-                Button(onClick = { /*TODO*/ }) {
-                    Text(text = "editar rótulos" )
+                IconButton(
+                    onClick = {  },
+                    modifier = Modifier.background(MaterialTheme.colorScheme.secondary, CircleShape).clip(
+                        CircleShape)
+                ) {
+                    Icon(
+                        imageVector = ImageVector.vectorResource(id = R.drawable.label_edit_icon),
+                        contentDescription = "editar rótulos",
+                        tint = Color.White,
+                        modifier = Modifier.size(20.dp)
+                    )
                 }
             }
             Spacer(modifier = Modifier.height(5.dp))
